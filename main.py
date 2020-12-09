00fb367648acf5d4d52cb2792cdf50b0b0c186d1
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2020-12-09 21:50:06 +0100

    Token .env Update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
+
 load_dotenv()
 
 intents = discord.Intents.all()
@@ -12,5 +13,5 @@ for filename in os.listdir("cogs"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run("Nzc2NjI5MzY5NDQ3MjUyMDE4.X63qdg.GI7N6pQV_yFFpiJXCUO4VhA0BK4")
-
+token = os.getenv('DISCORD_TOKEN')
+bot.run(token)
