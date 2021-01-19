97d4f598f7a94f234013494c244c87be2c740461
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2021-01-19 05:40:01 +0100

    Delete playground.py

--- playground.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-it = requests.post("https://id.twitch.tv/oauth2/token?client_id=qw6mmjkdrazpzl4odsmgj40bd6i7wd&client_secret=bb87yhllvarj73h4c61ttqskzmoefn")
-print(it.content)
-#print(it.headers)
-
-
