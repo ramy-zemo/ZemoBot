a5c7013dae12ec09a40b0cbb5194499e33c81260
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2021-01-21 11:08:48 +0100

    Deleted IC

--- etc/sql_reference.py
+++ etc/sql_reference.py
@@ -60,7 +60,6 @@ def insert_user_xp(guild_id, user, xp):
 
 
 def update_user_xp(guild_id, user, new_xp):
-    ic(guild_id, user, new_xp)
     sql = "UPDATE LEVEL SET xp=%s WHERE server=%s AND user=%s"
     val_1 = (new_xp, str(guild_id), str(user))
 
