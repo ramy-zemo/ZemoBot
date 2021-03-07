6776af76cc1a1b1a96ffe67f473bb864a9dc91da
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2021-03-07 10:31:40 +0100

    Update and rename config.py to config_sample.py

---
+++ config_sample.py
@@ -0,0 +1,14 @@
+# Rename File to config.py
+DISCORD_TOKEN = ""
+ADMIN_IDS = []
+ICON_URL = ""
+
+DB_IP = ""
+DB_USER = ""
+DB_PASSWORD = ""
+DB_DATABASE = ""
+
+TWITCH_CLIENT_ID = ""
+TWITCH_CLIENT_SECRET = ""
+
+BITLY_API = ""
